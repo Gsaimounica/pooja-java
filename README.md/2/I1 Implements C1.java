2b8ce4ef6e9121b2e31ec39536faaf23a472ccd3
Gsaimounica <godithipoojasaimounica.25.csm@anits.edu.in>
2026-08-29 13:57:04 +0530

    Create I1 Implements C1.java

---
+++ README.md/2/I1 Implements C1.java
@@ -0,0 +1,3 @@
+Result: Compile-time error (interface expected here).
+
+Reason: Interfaces define contracts and cannot inherit implementation from a class. An interface can never implement or extend a class.
