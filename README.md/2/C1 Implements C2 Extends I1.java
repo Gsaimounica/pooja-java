460fd4c30bcd1a0a031918c458c465f041a4ecdc
Gsaimounica <godithipoojasaimounica.25.csm@anits.edu.in>
2026-08-29 13:55:02 +0530

    Create C1 Implements C2 Extends I1.java

---
+++ README.md/2/C1 Implements C2 Extends I1.java
@@ -0,0 +1,3 @@
+Result: Compile-time error (Syntax error).
+
+Reason: The correct order and keywords must be used. A class must use extends before implements, and it extends a class and implements an interface.
