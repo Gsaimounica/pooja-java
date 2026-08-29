65495cb5a6428dd524b8d7aed37732e25adf1829
Gsaimounica <godithipoojasaimounica.25.csm@anits.edu.in>
2026-08-29 13:47:06 +0530

    Document multiple inheritance restriction in Java
    Added explanation for compile-time error when extending multiple classes in Java.

---
+++ README.md/2/C1 Extends C2,C3.java
@@ -0,0 +1,3 @@
+Result: Compile-time error (Class cannot extend multiple classes).
+
+Reason: Java does not support multiple inheritance with classes to avoid ambiguity (the Diamond Problem). A class can only extend one class.
