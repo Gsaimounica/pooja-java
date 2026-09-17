938f536ed98c4e604fb8c08b119f3582b0081173
Gsaimounica <godithipoojasaimounica.25.csm@anits.edu.in>
2026-09-17 09:16:39 +0530

    Add EqualsIgnoreCase example in Java

---
+++ Assignments/3/Equals Ignore Case.java
@@ -0,0 +1,9 @@
+package string;
+
+class StringEqualsIgnoreCase {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "JAVA";
+        System.out.println(a.equalsIgnoreCase(b));
+    }
+}
