fd8a1a165ca4d49d2f091c12c4d12f0226d3cb54
Gsaimounica <godithipoojasaimounica.25.csm@anits.edu.in>
2026-09-17 09:17:26 +0530

    Create Concat.java

---
+++ Assignments/3/Concat.java
@@ -0,0 +1,9 @@
+package string;
+
+class StringConcat {
+    public static void main(String[] args) {
+        String a = "Hello ";
+        String b = "Java";
+        System.out.println(a.concat(b));
+    }
+}
