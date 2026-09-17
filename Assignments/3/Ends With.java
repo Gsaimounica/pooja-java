38d0b035f3d525a99a9bff2b9296e2f83b9240b7
Gsaimounica <godithipoojasaimounica.25.csm@anits.edu.in>
2026-09-17 09:24:30 +0530

    Add Ends With example in Ends With.java

---
+++ Assignments/3/Ends With.java
@@ -0,0 +1,8 @@
+package string;
+
+class StringEndsWith {
+    public static void main(String[] args) {
+        String s = "Java Programming";
+        System.out.println(s.endsWith("ing"));
+    }
+}
