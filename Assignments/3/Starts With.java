9720ee5f8e5a8ff8ced664b0b0bc32e29d538df3
Gsaimounica <godithipoojasaimounica.25.csm@anits.edu.in>
2026-09-17 09:23:14 +0530

    Add Starts With example in Java

---
+++ Assignments/3/Starts With.java
@@ -0,0 +1,8 @@
+package string;
+
+class StringStartsWith {
+    public static void main(String[] args) {
+        String s = "Java Programming";
+        System.out.println(s.startsWith("Java"));
+    }
+}
